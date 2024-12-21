5fbe58d325c3c65918a12f5307e5c2fe5383115b
Aniruth_19 <m.aniruthan@gmail.com>
2024-12-21 20:51:07 +0530

    iterative code

--- fibo.py
+++ fibo.py
@@ -1,10 +1,13 @@
-def fibonacci_recursive(n):
+def fibonacci_iterative(n):
     if n <= 0:
         return 0
     elif n == 1:
         return 1
-    else:
-        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+    
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
 
 n = 10
-print(fibonacci_recursive(n))
+print(fibonacci_iterative(n))
